69285713bf998fe8ff0975b7cf8748f4632e4a5b
jaameyaw <johnsonameyaw22@gmail.com>
2025-10-24 01:27:50 +0000

    Solutution: Problem 1
    add twoSum function implementation to solve the problem.
    Replaces previous loop-based solution with a new twoSum function that checks for valid input and returns the indices of two numbers that sum to the target. Removes example variables and updates the console log for demonstration.

--- script.js
+++ script.js
@@ -12,16 +12,23 @@ For the purposes of this kata, some tests may have multiple answers; any valid s
 The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
 
 twoSum([1, 2, 3], 4)  returns [0, 2] or [2, 0]
-twoSum([3, 2, 4], 6) returns [1, 2] or [2, 1] */
+twoSum([3, 2, 4], 6) returns [1, 2] or [2, 1
+*/
+// Solution:
+function twoSum (numbers, target) {
+    if (numbers.filter(num => Number.isInteger(num)) && numbers.length >= 2) {
+        for (let i = 0; i < numbers.length; i++) {
+            for (let j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] === target) {
+                    return [i, j]
+                } 
+            }
 
-const numbers = [10, 20, 10, 40, 50, 60, 70];
-const target = 50;
-
-
-for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-        if (numbers[i] + numbers[j] === target) {
-            console.log([i, j]);
         }
+    } else {
+        return "Invalid Input"
     }
+
+    return n;
 }
+console.log (twoSum([1, 2, 3], 4)); // returns [0, 2] or [2, 0]
