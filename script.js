a8ba90fa9710bb3df224baa08a5ab04ea48b7e8c
jaameyaw <johnsonameyaw22@gmail.com>
2025-10-26 01:22:43 +0000

    retried problem 2 again

--- script.js
+++ script.js
@@ -45,18 +45,20 @@ For example, if n = 5, the output should be:
 */
 
 function printPyramid (n) {
-    for (let i = 1; i <= n; i++) {
+    for (let i = 1; i <= n; i++ ) {
         let row = "";
-        for (let j =1; j<= n-i; j++) {
-            row += " " ;
-        }
-        for (let k=1; k<= 2 * i - 1; k++) {
-            row += "*"
+
+        for (let j = 1; j <= n-i; j++) {
+            row += " ";
         }
 
-        console.log (row)
+        for (let k= 1; k <= 2*i-1; k++) {
+            row += "*";
+        }
+        
+        console.log(row)
     }
-
 }
+
 
 printPyramid(5)
