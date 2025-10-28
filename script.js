5cf97020c9e369331b4f694f996a7d286a319626
jaameyaw <johnsonameyaw22@gmail.com>
2025-10-28 01:06:09 +0000

    Problem 3 solution

--- script.js
+++ script.js
@@ -92,3 +92,20 @@ for (let j =1; j<=10; j++) {
 
     console.log(multiplesofFive)   
 } */
+
+//solution  
+function solution(number) {
+    if (number < 0) return 0;
+
+    let sum = 0;
+    for (let i = 1; i < number; i++) {
+       if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+       }
+    }
+    return sum
+
+    
+}
+
+console.log (solution(20))
