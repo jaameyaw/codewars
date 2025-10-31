fd38270b4a060043df177ecad220620996d35328
jaameyaw <johnsonameyaw22@gmail.com>
2025-10-31 01:37:54 +0000

    Break sentence to next line

--- script.js
+++ script.js
@@ -135,7 +135,8 @@ Unfortunately he is not good in maths.
 Can you help him to find out, how many cakes he could bake considering his recipes?
 
 Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). 
-For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). 
+Ingredients that are not present in the objects, can be considered as 0.
 */
 
 
